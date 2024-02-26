25267f28ed1b453b890aed5f8156e887b06d53fb
Tobie Morgan Hitchcock <tobie@surrealdb.com>
2024-02-26 21:51:08 +0000

    Update chocolatey update script

--- patch.ps1
+++ patch.ps1
@@ -1,38 +1,45 @@
 #!/usr/bin/env pwsh
 # Copyright (C) 2022 - ProhetLamb
+# Copyright (C) 2024 - tobiemh
 # Downloads the current surreal binary and patches the choco package information.
 
-$toolsdir = './tools'
-$version= $env:SURREALDB_VERSION
+$version = $env:SURREALDB_VERSION
 
-##
-# download the current binary
-##
+## --------------------------------------------------
+## Download the current binary and checksum
+## --------------------------------------------------
 
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 $baseurl='https://download.surrealdb.com'
 $arch="windows-amd64"
-$bin="$toolsdir/surreal.exe"
+$bin="tools/surreal.exe"
 $url="$baseurl/$version/surreal-$version.$arch.exe"
+$chk="$baseurl/$version/surreal-$version.$arch.txt"
 
 echo "Downloading $url to $bin"
-# https://download.surrealdb.com/v1.0.0-beta.7/surreal-v1.0.0-beta.7.windows-amd64.exe
 Invoke-WebRequest $url -UseBasicParsing -OutFile $bin
 
-$checksum=checksum -t sha256 -f $bin
+echo "Fetching $chk checksum"
+$checksum = checksum -t sha256 -f $bin
 
-##
-# patch checksum and checksum64 for the binary in powershell scripts
-##
+## --------------------------------------------------
+## Update the binary version in the version file
+## --------------------------------------------------
 
-# replaces the value of the following with $checksum
-#   checksum      =
-#   checksum64    = 'wegwerghwrgh'
-echo "Patching checksums in $toolsDir/*.ps1 to $checksum"
+echo "Updating version in current.version to $version"
+
+Write-Output $version | out-file -encoding utf8 "current.version"
+
+## --------------------------------------------------
+## Patch the checksum in the powershell scripts
+## --------------------------------------------------
+
+echo "Patching checksums in tools/*.ps1 to $checksum"
+
 $expr='^(\s*checksum\d*\s*=\s*).*'
 $replace='$1'+"'$checksum'"
 
-foreach($file in gci "$toolsDir/*.ps1") {
+foreach($file in gci "tools/*.ps1") {
   [array]$content = Get-Content $file
   for($i = 0; $i -lt $content.Count; $i++) {
     $content[$i] = $content[$i] -replace $expr, $replace
@@ -40,32 +47,26 @@ foreach($file in gci "$toolsDir/*.ps1") {
   Set-Content $file $content
 }
 
-##
-# update current.version
-##
+## --------------------------------------------------
+## Update the package version in the nuspec file
+## --------------------------------------------------
 
-echo $version > current.version
+$version = $version -replace '^v(\d+\.\d+\.\d+)$', '$1'
 
-##
-# update version for surrealdb.nuspec
-##
-
-# patch beta version string to conform with
-# https://docs.microsoft.com/en-us/nuget/concepts/package-versioning#creating-prerelease-packages
-# rplacesing v1.0.0-beta.7 with 1.0.0-beta7
-$version = $version -replace '^v(\d+\.\d+\.\d+)-beta\.(\d+).*$', '$1-beta$2'
-
-# replaces the following
-#    <version>1.0.0</version>
-#    <version>1.0.0-beta5</version>
 echo "Patching version in surreal.nuspec to $version"
+
+$file='surreal.nuspec'
 $expr='<version>\d+\.\d+\.\d+(?:-\w+\d*)?</version>'
 $replace="<version>$version</version>"
 
-$file='surreal.nuspec'
 [array]$content = Get-Content $file
 for($i = 0; $i -lt $content.Count; $i++) {
   $content[$i] = $content[$i] -replace $expr, $replace
 }
 Set-Content $file $content
+
+## --------------------------------------------------
+## All complete
+## --------------------------------------------------
+
 echo "Done"
